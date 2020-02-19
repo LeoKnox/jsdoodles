0a023db529edf1ff42603b4f73d160483f596ab5
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-18 20:23:38 -0800

    Update dndrolls.js

--- dndrandom/dndrolls.js
+++ dndrandom/dndrolls.js
@@ -6,11 +6,15 @@ function roll(attacks, damage, modifier)
       sum += Math.floor(Math.random()*damage)+modifier+1;
     }
   return sum;
-}
+};
+
+sum1 = 0;
+sum2 = 0;
 
 for (var x = 0; x < 50; x++)
   {
-
+    sum1 += roll(2, 6, 3);
+    sum2 += roll(1, 8, 5);
   }
-x = roll(1, 10, 0);
-console.log(`number ${x}`);
+console.log(`1 attack 2d6 +3 bonus ${sum1}`);
+console.log(`1 attack 1d8+2 +3 bonus ${sum2}`);
